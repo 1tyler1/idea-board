bdca896ed71727989b74f5a0e99d6630012b4eb3
1tyler1 <mya12ga@yahoo.com>
2018-04-24 12:28:57 -0400

    updating classwork

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.get('/', (req,res) => {
   res.sendFile(__dirname + '/client/build/index.html')
 })
 
+const UsersController = require('./controllers/users')
+app.get('/api/users', UsersController)
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
