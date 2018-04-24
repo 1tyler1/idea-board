92f7dc7ffbd529ba523fcd7fd97eebfdc76763b2
1tyler1 <mya12ga@yahoo.com>
2018-04-24 16:21:15 -0400

    update classwork

--- client/src/components/IdeaPage.js
+++ client/src/components/IdeaPage.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
 
 class IdeaPage extends Component {
-  render() {
+  state = {
+    user: {
+      userName: 'Bob'
+    },
+    ideas: [{
+      id: 1,
+      title: 'hello',
+      description: 'world'
+    }, {
+      id: 2,
+      title: 'hola',
+      description: 'mundo'
+    }, {
+      id: 3,
+      title: 'goodnight',
+      description: 'moon'
+    }, {
+      id: 4,
+      title: 'greetings',
+      description: 'earthlings'
+    }]
+  }
+
+  handleChange = (event) => {
+    console.log(event)
+  }
+
+  render () {
+   const ideas = this.state.ideas.map((idea, i) => {
+          return (
+              <div key={i}>
+                <input type="text" name="title" value={idea.description} onChange={this.handleChange}/>
+                <textarea name="description" onChange={this.handleChange}/>
+                <button>Delete Idea</button>
+              </div>
+            )
+          })
     return (
       <div>
-        IdeaPage
-      </div>
+        <div>
+          <h1>{this.state.user.userName}'s Idea Board</h1>
+          <button>New Idea</button>
+        </div>
+        
+        </div>
+    
     )
   }
 }
+   
 
 export default IdeaPage
