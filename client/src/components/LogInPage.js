d0fddad42b3a526d7006bf1473eb918202bb5674
1tyler1 <mya12ga@yahoo.com>
2018-04-24 15:03:29 -0400

    updating classwork

--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -1,15 +1,42 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
-class LogInPage extends Component{
-    render() {
-        return(
-            <div>
-                LogInPage
-                <Link to='/'>Home</Link>
-            </div>
-        )
-    }
+class LogIn extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount(){
+      this.getAllUsers()
+  }
+
+  getAllUsers = () => {
+    axios.get('/api/users')
+    .then(res => {
+        console.log('saving users to state', res.data)
+      this.setState({users: res.data})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+  }
+  
+  render () {
+      console.log('saving users to state', this.state.users)
+      const userLinks =  this.state.users.map((user, i) => {
+    return (<Link to={`/user/${user._id}`}>{user.userName}</Link>) })
+    
+    return (
+      <div>
+      <Link to='/'>Home</Link>
+        <h1>Log-In</h1>
+        <h3>Please Select an Existing User</h3>
+       {userLinks}
+       
+      </div>
+    )
+  }
 }
 
-export default LogInPage
+export default LogIn
--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   User.find().then(users => {
+    console.log("USERS LISTED", users)
     res.json(users)
   })
   .catch((err) => console.log(err))
