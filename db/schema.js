6136263e077e94bcbaeeb7f24589ecc7750d741a
1tyler1 <mya12ga@yahoo.com>
2018-04-24 12:01:47 -0400

    udpating classwork

--- db/schema.js
+++ db/schema.js
@@ -1,3 +1,25 @@
 const mongoose = require ('mongoose')
 
 const schema = mongoose.schema
+
+const Userschema = new Schema ({
+    Username: String,
+    Password: String,
+    Ideas:[IdeasSchema]
+
+})
+
+const IdeaSchema = new Schema ({
+    Title: String,
+    Description: String,
+    Created: Date
+})
+
+const UserModel = mongoose.model('User', UserSchema)
+const IdeaModel = mongoose.model('Idea', IdeaSchema)
+
+// Export each model so they can be required elsewhere
+module.exports = {
+    UserModel: UserModel,
+    IdeaModel: IdeaModel
+}
